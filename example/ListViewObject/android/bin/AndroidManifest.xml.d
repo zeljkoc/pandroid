69d2353504d0d5282a4eafe7114d4ab5c4ca0b94
root <zeljkoc@users.noreply.github.com>
2016-07-13 19:38:12 +0200

    add andorid/src

---
+++ example/ListViewObject/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/usr/local/pandroid/example/ListViewObject/android/bin/AndroidManifest.xml : \
+/usr/local/pandroid/example/ListViewObject/android/AndroidManifest.xml \
---
+++ example/ListViewObject/android/bin/ListViewObject-release.apk.d
@@ -0,0 +1,6 @@
+/usr/local/pandroid/example/ListViewObject/android/bin/ListViewObject-release.apk : \
+/usr/local/pandroid/example/ListViewObject/android/bin/ListViewObject.ap_ \
+/usr/local/pandroid/example/ListViewObject/android/bin/classes.dex \
+/usr/local/pandroid/example/ListViewObject/android/src/zc \
+/usr/local/pandroid/example/ListViewObject/android/libs/TIniFile.jar \
+/usr/local/pandroid/sdk/tools/support/annotations.jar \

081ef7141dffa31aba3bfbff690c9cdaabce339f
root <zeljkoc@users.noreply.github.com>
2016-08-12 08:07:29 +0200

    Pandroid v 1.0 * GetLazarus v. 1.7

---
+++ example/TempFirebird/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/usr/local/pandroid/example/TempFirebird/android/bin/AndroidManifest.xml : \
+/usr/local/pandroid/example/TempFirebird/android/AndroidManifest.xml \
---
+++ example/TempFirebird/android/bin/TempFirebird-release.apk.d
@@ -0,0 +1,7 @@
+/usr/local/pandroid/example/TempFirebird/android/bin/TempFirebird-release.apk : \
+/usr/local/pandroid/example/TempFirebird/android/bin/TempFirebird.ap_ \
+/usr/local/pandroid/example/TempFirebird/android/bin/classes.dex \
+/usr/local/pandroid/example/TempFirebird/android/src/zc \
+/usr/local/pandroid/example/TempFirebird/android/libs/TIniFile.jar \
+/usr/local/pandroid/sdk/tools/support/annotations.jar \
+/usr/local/pandroid/example/TempFirebird/android/libs/zcfbclient.jar \

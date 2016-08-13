fd2e3ee422497a938e69db9c831ec9a998964c2d
root <zeljkoc@users.noreply.github.com>
2016-08-13 23:28:36 +0200

    Pandroid v 1.0 * GetLazarus v. 1.7

--- project/Popis/android/bin/Popis-release.apk.d
+++ project/Popis/android/bin/Popis-release.apk.d
@@ -1,6 +1,7 @@
 /usr/local/pandroid/project/Popis/android/bin/Popis-release.apk : \
 /usr/local/pandroid/project/Popis/android/bin/Popis.ap_ \
 /usr/local/pandroid/project/Popis/android/bin/classes.dex \
+/usr/local/pandroid/project/Popis/android/src/zc \
 /usr/local/pandroid/project/Popis/android/libs/TIniFile.jar \
 /usr/local/pandroid/project/Popis/android/libs/achartengine-1.1.0.jar \
 /usr/local/pandroid/sdk/tools/support/annotations.jar \

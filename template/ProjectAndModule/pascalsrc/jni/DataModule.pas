48558ed6084d29459e943f9342e760d4de5c8c2c
Zeljko Cvijanovic <cvzeljko@gmail.com>
2017-02-12 13:55:06 +0100

    template PModule params

--- template/ProjectAndModule/pascalsrc/jni/DataModule.pas
+++ template/ProjectAndModule/pascalsrc/jni/DataModule.pas
@@ -12,13 +12,14 @@ uses
 type
   TDataM = class(TDataModule)
   private
-    FTest: string;
+
   private
     { private declarations }
+    procedure SetParameters(Value: String);
   public
     { public declarations }
   published
-    property Test: String read FTest write FTest;
+    property Parameters: String write SetParameters;  
   end;
 
 {var
@@ -28,7 +29,30 @@ implementation
 
 {$R *.lfm}
 
+{ TDataM }
 
+procedure TDataM.SetParameters(Value: String);
+var
+  parametri: TStringList;
+  i: integer;
+begin
+  parametri:= TStringList.Create;
+  try
+    parametri.StrictDelimiter := true;
+    parametri.Delimiter := ';';    
+    parametri.DelimitedText := Value; // Value := 'user=SYSDBA;password=masterkey'
+
+    for i:=0 to parametri.Count -1 do begin
+      case parametri.Names[i] of
+           '': parametri.ValueFromIndex[i];
+      end;
+    end;
+
+  finally
+    parametri.Free;
+  end;
+
+end; 
 
 end.
 
